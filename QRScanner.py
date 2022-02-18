d8b993eb28c80603252f3ef1b57104e4ba60f835
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2022-02-18 18:08:31 +0800

    Imported pyzbar Module - Added lines for BoundingBoxes; Replaced QRDetect function

--- QRScanner.py
+++ QRScanner.py
@@ -1,6 +1,7 @@
 import cv2 as CV
 import webbrowser as web
-import datetime, os
+import datetime, os, numpy
+from pyzbar import pyzbar
 
 # ---------------- CONTEXT --------------------
 # Program: Contact Tracing App
@@ -26,34 +27,39 @@ qrCapt = CV.VideoCapture(0)
 qrCapt.set(3, 650)
 qrCapt.set(4, 480)
 
-img_QRdetect = CV.QRCodeDetector()
-
 while True:
-    _, image = qrCapt.read()
+    etc_, image = qrCapt.read()
     
-    QRData, CoordBox, _ = img_QRdetect.detectAndDecode(image)
-    if QRData:
-        DecodedDataQR = QRData
-        try:
-            verifyFl = os.path.exists("QR_Logs.txt")
-            if verifyFl == True:
-                    verifyCtnt = os.path.getsize("QR_Logs.txt")
-                    if verifyCtnt == 0:
+    for QRPattern in pyzbar.decode(image):
+        QRData = QRPattern.data.decode('utf-8')
+        boxPnt = numpy.array([QRPattern.polygon],numpy.int32)
+        boxPnt = boxPnt.reshape((-1, 1, 2))
+        CV.polylines(image, [boxPnt], True, (0, 255, 0), 4)
+
+        boxPnt0 = QRPattern.rect
+        CV.putText(image, QRData, (boxPnt0[0], boxPnt0[1]), CV.FONT_HERSHEY_COMPLEX_SMALL, 0.9, (0, 15, 0), 2)
+        if QRData:
+            DecodedDataQR = QRData
+            try:
+                verifyFl = os.path.exists("QR_Logs.txt")
+                if verifyFl == True:
+                        verifyCtnt = os.path.getsize("QR_Logs.txt")
+                        if verifyCtnt == 0:
+                            StoreDatatoTxt(DecodedDataQR, 0)
+                            break
+                        else:
+                            StoreDatatoTxt(DecodedDataQR, 1)
+                            break
+                elif verifyFl == False:
                         StoreDatatoTxt(DecodedDataQR, 0)
                         break
-                    else:
-                        StoreDatatoTxt(DecodedDataQR, 1)
-                        break
-            elif verifyFl == False:
-                    StoreDatatoTxt(DecodedDataQR, 0)
-                    break
-        except Exception as e:
-            print(f"\33[91mAn error occured :( ---> {e}\33[0m")
-            break
+            except Exception as e:
+                print(f"\33[91mAn error occured :( ---> {e}\33[0m")
+                break
     CV.imshow("QRCODE_Scanner", image)
-    if CV.waitKey(1) == ord("s"):
-        break
+    CV.waitKey(1)
+    break
 
-bb = web.open(str(DecodedDataQR))
+redirectBrwsr = web.open(DecodedDataQR)
 qrCapt.release()
 CV.destroyAllWindows()
